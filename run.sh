141bae9714c6a2fa9020f7e46f7654aa03d71f48
yyhhenry <yyhhenry@foxmail.com>
2024-09-09 12:20:18 +0800

    better env

--- run.sh
+++ run.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 export OSLAB_PATH=$(dirname `which $0`)
+export BOCHS_PATH="$OSLAB_PATH/env/bochs"
 
-if [ ! $1 -e "skip-build" ]; then
-	make all -C $OSLAB_PATH
-fi
+make -C $OSLAB_PATH
 
 if [ ! -e "$OSLAB_PATH/env/hdc/umounted" ]; then
 	echo umount env/hdc first
-	sudo umount $OSLAB_PATH/env/hdc
-	if [ "$?" != "0" ]; then
-		exit
-	fi
+	exit 1
 fi
 
-$OSLAB_PATH/env/bochs/bochs-gdb -q -f $OSLAB_PATH/env/bochs/bochsrc.bxrc
+$BOCHS_PATH/bochs-gdb -q -f $BOCHS_PATH/bochsrc.bxrc
