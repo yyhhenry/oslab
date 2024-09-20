8ddabaa2cc59a2e7be3ad6d4a847f7c779c65b02
yyhhenry <yyhhenry@foxmail.com>
2024-09-20 18:54:04 +0800

    feat: mount

--- mount.sh
+++ mount.sh
@@ -2,4 +2,6 @@
 if [ -e "env/hdc/umounted" ]; then
     rm env/hdc/umounted
 fi
-sudo guestmount -a hdc-0.11.img -m /dev/sda1 ./env/hdc
+export LIBGUESTFS_BACKEND=direct
+# Try `libguestfs-test-tool` if you have any problem
+guestmount -a hdc-0.11.img -m /dev/sda1 ./env/hdc
