1115bf860b2613bd7bdbc4e1c224300fe856d924
yyhhenry <yyhhenry@foxmail.com>
2024-09-20 16:12:48 +0800

    feat: 添加xmake构建镜像的脚本

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 export OSLAB_PATH=$(dirname `which $0`)
 export BOCHS_PATH="$OSLAB_PATH/env/bochs"
 
+xmake build image
+
 if [ ! -e "$OSLAB_PATH/env/hdc/umounted" ]; then
 	echo umount env/hdc first
 	sudo umount $OSLAB_PATH/env/hdc
