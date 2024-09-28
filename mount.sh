611a2a9259fede548688c3463dddfc374aeaae1d
yyhhenry <yyhhenry@foxmail.com>
2024-09-28 17:46:12 +0800

    feat: mount

--- mount.sh
+++ mount.sh
@@ -2,5 +2,6 @@
 export OSLAB_PATH=$(dirname `which $0`)
 export MNT_PATH="$OSLAB_PATH/env/hdc"
 
-$OSLAB_PATH.env/umount_if_needed.sh
+$OSLAB_PATH/env/umount_if_needed.sh
+echo "mount env/hdc"
 sudo mount -o loop,offset=1024 $OSLAB_PATH/hdc-0.11.img $MNT_PATH
