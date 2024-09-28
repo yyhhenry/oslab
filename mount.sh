5ec86167a55f4352fbadeff519ac3dcbd68c9f4a
yyhhenry <yyhhenry@foxmail.com>
2024-09-28 17:45:11 +0800

    feat: mount

--- mount.sh
+++ mount.sh
@@ -2,11 +2,5 @@
 export OSLAB_PATH=$(dirname `which $0`)
 export MNT_PATH="$OSLAB_PATH/env/hdc"
 
-if [ -e "$MNT_PATH/umounted" ]; then
-    rm $MNT_PATH/umounted
-else
-    echo "umount env/hdc first"
-    exit
-fi
-
+$OSLAB_PATH.env/umount_if_needed.sh
 sudo mount -o loop,offset=1024 $OSLAB_PATH/hdc-0.11.img $MNT_PATH
