4daaee68e8c07bfc570df5236b6a18faedc8bc5f
yyhhenry <yyhhenry@foxmail.com>
2024-09-28 17:46:39 +0800

    Refactor exp2.sh to use reset_img.sh for image reset and improve file copying

--- exp2/exp2.sh
+++ exp2/exp2.sh
@@ -2,7 +2,7 @@
 export EXP2_PATH=$(dirname `which $0`)
 export OSLAB_PATH="$EXP2_PATH/.."
 
-cp $OSLAB_PATH/env/hdc-0.11.img.bak $OSLAB_PATH/hdc-0.11.img
+$OSLAB_PATH/env/reset_img.sh
 $OSLAB_PATH/mount.sh
 cp $OSLAB_PATH/linux-0.11/include/unistd.h $OSLAB_PATH/env/hdc/usr/include/
 cp $OSLAB_PATH/exp2/home/* $OSLAB_PATH/env/hdc/usr/root/
